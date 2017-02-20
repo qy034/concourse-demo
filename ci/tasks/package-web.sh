7c612a0bcc486b55052ea5094116562b73229761
git <git>
2017-02-20 17:50:02 +0800

    shell bug fix

--- ci/tasks/package-web.sh
+++ ci/tasks/package-web.sh
@@ -3,4 +3,4 @@
 cd source-code/concourse-demo-web/
 ./mvnw package
 
-cp target/concourse-demo-web*.jar target-web/concourse-demo-web.jar
+cp target/concourse-demo-web*.jar ../../target-web/concourse-demo-web.jar
